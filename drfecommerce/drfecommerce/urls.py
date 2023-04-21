21b2ee19c4672c8f30172ee44958dbee831ff912
Biswas <ibiswas@apac.corpdir.net>
2023-04-21 23:53:32 +0530

    ,commit 3

--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from product import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register("category", views.CategoryViewSet)
+router.register("brand", views.BrandViewSet)
+router.register("product", views.ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(),name="schema"),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
